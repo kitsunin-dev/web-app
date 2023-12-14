dd4f831dc1cb57612d82eeabb10d5f4fbef44518
kitsunin-dev <stepanpilipcenko@gmail.com>
2023-12-14 16:09:02 +0300

    Init commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    name = request.args.get("name")
+    message = request.args.get("message")
+    return render_template("index.html", name=name, message=message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
