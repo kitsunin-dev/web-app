7fe617cb0ff9b4db6a773a1a025d936acafc6c1a
sindwrr <87270185+sindwrr@users.noreply.github.com>
2024-01-12 18:02:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template, request
+from random import randint
+
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
+refresh = 0
 
-@app.route("/")
-def hello():
-    name = request.args.get("name")
-    message = request.args.get("message")
-    return render_template("index.html", name=name, message=message)
+
+@app.route("/code/?username=<username>&password=<password>")
+def code(username, password):
+    global refresh
+    refresh += 1
+    if refresh > 1:
+        refresh = 0
+        return redirect(url_for("survey"))
+
+    if username == "admin" and password == "password":
+        return str(randint(0, 9999)).zfill(4)
+    return "Неверные данные! Повторите попытку авторизации."
+
+
+@app.route("/", methods=["GET"])
+def survey():
+    user = request.args.get("username")
+    psw = request.args.get("password")
+    if user and psw:
+        return redirect(url_for("code", username=user, password=psw))
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=True)
